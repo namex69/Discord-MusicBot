aac4c265079f9b439fb276e1ff09679a4b307185
NameX <v4nx3m@gmail.com>
2022-03-01 19:56:42 +0900

    feat: Change EmbedColor

--- botconfig.js
+++ botconfig.js
@@ -15,14 +15,14 @@ module.exports = {
   CookieSecret: "Pikachu is cute", //A Secret like a password
   IconURL:
     "https://i.postimg.cc/5y0cb9CK/spinning-vinyl.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
-  EmbedColor: "RANDOM", //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
+  EmbedColor: "0x2f3136", //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
   Permissions: 2205281600, //Bot Inviting Permissions
   Website: process.env.Website || "https://example.com", //Website where it is hosted at includes http or https || Use "0.0.0.0" if you using Heroku || Do not include /api/callback. Just the website url. I.e. "https://foo.bar"
   //If you get invalid oauth, make sure on the discord developer page you set the oauth url to something like: https://example.com/api/callback.
   
   Presence: {
     status: "online", // You can show online, idle, and dnd
-    name: "Music", // The message shown
+    name: "Music with NameX", // The message shown
     type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING
   },
 
